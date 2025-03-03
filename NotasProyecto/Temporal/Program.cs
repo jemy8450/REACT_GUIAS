09e8521f90449f75522e8784e8ff1c6f2d6a09dc
jemy8450 <126819583+jemy8450@users.noreply.github.com>
2025-03-02 23:46:59 -0600

    Guia 3

---
+++ NotasProyecto/Temporal/Program.cs
@@ -0,0 +1,16 @@
+﻿using reatBackend.Repository;
+using System;
+
+AlumnoDAO alumnoDAO = new AlumnoDAO();
+
+var alumno = alumnoDAO.SelectAll();
+
+foreach (var item in alumno) 
+{
+    Console.WriteLine(item.Nombre);
+}
+
+Console.WriteLine("");
+
+var selectById = alumnoDAO.GetById(1000);
+Console.WriteLine(selectById?.Nombre);
---
+++ NotasProyecto/reatBackend/Repository/AlumnoDAO.cs
@@ -0,0 +1,28 @@
+﻿using reatBackend.Context;
+using reatBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reatBackend.Repository
+{
+    public class AlumnoDAO
+    {
+        public RegistroAlumno2_0Context contexto = new RegistroAlumno2_0Context();
+
+        public List<Alumno> SelectAll()
+        {
+            var alumno = contexto.Alumnos.ToList<Alumno>();
+            return alumno;
+
+        }
+
+        public Alumno? GetById(int id)
+        {
+            var alumno = contexto.Alumnos.Where(x => x.Id == id).FirstOrDefault();
+            return alumno == null ? null : alumno;
+        }
+    }
+}
