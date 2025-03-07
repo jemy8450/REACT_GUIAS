9ee5f2c607514a21ad3fa0f631d7dc3cdadba9b8
jemy8450 <126819583+jemy8450@users.noreply.github.com>
2025-03-07 15:59:50 -0600

    Guia 4

--- NotasProyecto/Temporal/Program.cs
+++ NotasProyecto/Temporal/Program.cs
@@ -1,16 +1,50 @@
-﻿using reatBackend.Repository;
-using System;
-
-AlumnoDAO alumnoDAO = new AlumnoDAO();
-
-var alumno = alumnoDAO.SelectAll();
-
-foreach (var item in alumno) 
+﻿using reatBackend.Models;
+using reatBackend.Repository;
+//abstracion de un objeto Dao
+AlumnoDAO alumnoDao = new AlumnoDAO();
+//llamamos el metodo que creamos en el DAO 
+#region SelectAll
+var alumno = alumnoDao.SelectAll();
+// Recorremos la lista 
+foreach (var item in alumno)
 {
-    Console.WriteLine(item.Nombre);
+    Console.WriteLine(item.Id + " " + item.Nombre);
 }
-
-Console.WriteLine("");
-
-var selectById = alumnoDAO.GetById(1000);
+#endregion
+Console.WriteLine(" ");
+#region SelectByID
+// Provamos el select por Id
+var selectById = alumnoDao.GetById(1);
 Console.WriteLine(selectById?.Nombre);
+#endregion
+Console.WriteLine(" ");
+
+#region addAlumno
+var nuevoAlumno = new Alumno
+{
+    Direccion = "Chalatenango, Barrio el centro",
+    Dni = "1345",
+    Edad = 30,
+    Email = "12344321@email",
+    Nombre = "Ricardo JR Milos"
+};
+    var resultado = alumnoDao.insertarAlumno(nuevoAlumno);
+    Console.WriteLine(resultado);
+#endregion
+#region updateAlumno
+var nuevoAlumno2 = new Alumno
+{
+    Direccion = "Chalatenango, Barrio el centro",
+    Dni = "1345",
+    Edad = 30,
+    Email = "5@email",
+    Nombre = "Wiliams"
+};
+
+    var resultado2 = alumnoDao.update(2, nuevoAlumno2);
+    Console.WriteLine(resultado2);
+#endregion
+#region borrar
+var result = alumnoDao.deleteAlumno(1026);
+Console.WriteLine("Se elimino " + result);
+#endregion
--- NotasProyecto/reatBackend/Repository/AlumnoDAO.cs
+++ NotasProyecto/reatBackend/Repository/AlumnoDAO.cs
@@ -12,17 +12,99 @@ namespace reatBackend.Repository
     {
         public RegistroAlumno2_0Context contexto = new RegistroAlumno2_0Context();
 
+        //selectAll
         public List<Alumno> SelectAll()
         {
             var alumno = contexto.Alumnos.ToList<Alumno>();
             return alumno;
 
         }
-
+        //selectById
         public Alumno? GetById(int id)
         {
             var alumno = contexto.Alumnos.Where(x => x.Id == id).FirstOrDefault();
             return alumno == null ? null : alumno;
+        }
+
+        //Insert
+        public bool insertarAlumno(Alumno alumno)
+        {
+            try
+            {
+                var alum = new Alumno
+                {
+                    Direccion = alumno.Direccion,
+                    Edad = alumno.Edad,
+                    Email = alumno.Email,
+                    Dni = alumno.Dni,
+                    Nombre = alumno.Nombre
+                };
+                //añaddimos al contexto de dataset que representa la base de datos el metodo add
+                contexto.Alumnos.Add(alum);
+                //este elemnto en si no nos guardara los datos para ello debemos utilizar el metodo save
+                contexto.SaveChanges();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        //update alumno
+        public bool update(int id, Alumno actualizar)
+        {
+            try
+            {
+                var alumnoUpdate = GetById(id);
+
+                if (alumnoUpdate == null)
+                {
+                    Console.WriteLine("Alumno es null");
+                    return false;
+                }
+
+                alumnoUpdate.Direccion = actualizar.Direccion;
+                alumnoUpdate.Dni = actualizar.Dni;
+                alumnoUpdate.Nombre = actualizar.Nombre;
+                alumnoUpdate.Email = actualizar.Email;
+
+                contexto.Alumnos.Update(alumnoUpdate);
+                contexto.SaveChanges();
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.InnerException);
+                return false;
+            }
+        }
+
+        //Delete
+        public bool deleteAlumno(int id)
+        {
+            var borrar = GetById(id);
+            try
+            {
+                if (borrar == null)
+                {
+                    return false;
+
+                }
+                else
+                {
+                    contexto.Alumnos.Remove(borrar);
+                    contexto.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.InnerException);
+                return false;
+            }
         }
     }
 }
