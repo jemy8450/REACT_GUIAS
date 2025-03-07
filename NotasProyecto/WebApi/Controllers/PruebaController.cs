da41382aa8d57387b059fc428651008dbac35214
jemy8450 <126819583+jemy8450@users.noreply.github.com>
2025-03-07 17:41:06 -0600

    Guia 6

---
+++ NotasProyecto/WebApi/Controllers/PruebaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaController : ControllerBase
+    {
+        //indica la url
+        [HttpGet("prueba")]
+        //metodo publico que se ejecutara si la url es llamada
+        public string Get()
+        {
+          return "Hola Mundo";
+        }
+    }
+}
