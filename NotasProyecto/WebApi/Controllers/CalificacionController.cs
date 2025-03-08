a681e98ae972eaca9a50c76612eeec5c472301f5
jemy8450 <126819583+jemy8450@users.noreply.github.com>
2025-03-08 17:36:32 -0600

    Guia 12

---
+++ NotasProyecto/WebApi/Controllers/CalificacionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using reatBackend.Models;
+using reatBackend.Repository;
+
+namespace WebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CalificacionController : ControllerBase
+    {
+        private CalificacionDao _cdao = new CalificacionDao();
+
+        #region lista de calificaciones
+        [HttpGet("calificaciones")]
+
+        public List<Calificacion> get(int idMatricula)
+        {
+            return _cdao.seleccion(idMatricula);
+        }
+    }
+    #endregion
+}
---
+++ NotasProyecto/reatBackend/Repository/CalificacionDAO.cs
@@ -0,0 +1,49 @@
+﻿using reatBackend.Context;
+using reatBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reatBackend.Repository
+{
+    public class CalificacionDao
+    {
+         
+        public RegistroAlumno2_0Context _contexto = new RegistroAlumno2_0Context();
+
+        #region Seleccionar_lista_caificaciones 
+        public List<Calificacion> seleccion(int matriculaid)
+        {
+
+            var matricula = _contexto.Matriculas.Where(x => x.Id == matriculaid).ToList();
+            ;
+            try
+            {
+                if (matricula != null)
+                {
+
+                    var calificacion = _contexto.Calificacions.Where(x => x.Id == matriculaid).ToList();
+
+                    return calificacion;
+                }
+                else
+                {
+
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+
+
+        }
+        #endregion
+    }
+}
